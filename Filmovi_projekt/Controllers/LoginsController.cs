00e58f87d65e4d3d7abb848d772b8b46e62e1625
kfeltrin <kfeltrin@vub.hr>
2023-04-21 11:57:30 +0200

    Add project files.

---
+++ Filmovi_projekt/Controllers/LoginsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Filmovi.Models;
+
+namespace Filmovi_projekt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        private readonly LoginContext _context;
+
+        public LoginsController(LoginContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Logins
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Login>>> Getlogins()
+        {
+          if (_context.logins == null)
+          {
+              return NotFound("Login data not found");
+          }
+            return await _context.logins.ToListAsync();
+        }
+
+        // GET: api/Logins/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Login>> GetLogin(int id)
+        {
+          if (_context.logins == null)
+          {
+              return NotFound("Login data not found");
+          }
+            var login = await _context.logins.FindAsync(id);
+
+            if (login == null)
+            {
+                return NotFound("Searched user not found");
+            }
+
+            return login;
+        }
+
+        // PUT: api/Logins/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLogin(int id, Login login)
+        {
+            if (id != login.id_user)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(login).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoginExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Logins
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Login>> PostLogin(Login login)
+        {
+          if (_context.logins == null)
+          {
+              return Problem("Entity set 'LoginContext.logins'  is null.");
+          }
+            _context.logins.Add(login);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLogin", new { id = login.id_user }, login);
+        }
+
+        // DELETE: api/Logins/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLogin(int id)
+        {
+            if (_context.logins == null)
+            {
+                return NotFound("Login data not found");
+            }
+            var login = await _context.logins.FindAsync(id);
+            if (login == null)
+            {
+                return NotFound("Searched user not found or already deleted");
+            }
+
+            _context.logins.Remove(login);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LoginExists(int id)
+        {
+            return (_context.logins?.Any(e => e.id_user == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Filmovi_projekt/Models/LoginContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Filmovi.Models
+{
+    public class LoginContext:DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options):base(options) 
+        { 
+        
+        }
+        public DbSet<Login> logins { get; set; }
+    }
+}
