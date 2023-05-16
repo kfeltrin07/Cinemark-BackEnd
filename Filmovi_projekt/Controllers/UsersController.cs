365bbef16ad3bcaeadffc95a13e81fbc5b89af80
Erik <baukovacerik@gmail.com>
2023-05-16 10:16:45 +0200

    Login minor changes

--- Filmovi_projekt/Controllers/UsersController.cs
+++ Filmovi_projekt/Controllers/UsersController.cs
@@ -158,7 +158,7 @@ namespace Filmovi_projekt.Controllers
             if (login == null)
                 return BadRequest();
             var user = await _context.Users.FirstOrDefaultAsync(x => x.username == login.username);
-            
+
             if (user == null)
                 return NotFound(new { Message = "User not Found" });
             else if (!PasswordHasher.VerifyPassword(login.password, user.password))
@@ -166,19 +166,21 @@ namespace Filmovi_projekt.Controllers
             else if (user.verified == false)
                 return BadRequest(new { Message = "User is not activated" });
             else
-
-            user.token = CreateJwt(user);
-            var newAccessToken = user.token;
-            var newRefreshToken = CreateRefreshToken();
-            user.RefreshToken= newRefreshToken;
-            user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(30);
-            await _context.SaveChangesAsync();
-
-            return Ok(new TokenApiDto()
             {
-                AccessToken = newAccessToken,
-                RefreshToken = newRefreshToken
-            });
+
+                user.token = CreateJwt(user);
+                var newAccessToken = user.token;
+                var newRefreshToken = CreateRefreshToken();
+                user.RefreshToken = newRefreshToken;
+                user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(30);
+                await _context.SaveChangesAsync();
+
+                return Ok(new TokenApiDto()
+                {
+                    AccessToken = newAccessToken,
+                    RefreshToken = newRefreshToken
+                });
+            }
         }
 
         // GET: api/TestUsers/register
@@ -198,7 +200,7 @@ namespace Filmovi_projekt.Controllers
             login.password = PasswordHasher.HashPassword(login.password);
             login.activation_code = activationCode;
             login.role = "user";
-            login.RefreshTokenExpiryTime= DateTime.Now.AddMinutes(30);
+            login.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(30);
 
             try
             {
